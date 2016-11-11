01dd903d42dedb2e24ea3dd4e357fea887fd7f07
Gas9220 <gasparemonte@MacBook-Pro-di-Gaspare.local>
2016-11-11 13:52:05 +0100

    Initial Commit

---
+++ pokedexGas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedexGas
+//
+//  Created by Gaspare Monte on 11/11/16.
+//  Copyright © 2016 Gaspare Monte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
